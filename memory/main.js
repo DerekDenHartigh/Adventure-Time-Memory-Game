51dc524c0bbabed2f412df44bd95aef9a497858a
dave <d.gillespie221@gmail.com>
2019-03-20 20:42:12 -0400

    first branch commit

--- memory/main.js
+++ memory/main.js
@@ -1,2 +1,9 @@
 "use strict"
 
+
+// works kind of wonky... maybe with the avatar picture on the front it will work?
+$('.flip-card').on('click', function() {
+  $(this).css('transform', 'rotateY(180deg)');
+  console.log("this works");
+  console.log(this);
+})
