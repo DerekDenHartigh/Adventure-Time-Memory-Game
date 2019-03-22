63049db05517dc4edf038683a519844d87c283f3
dave <d.gillespie221@gmail.com>
2019-03-21 21:23:13 -0400

    saving before I mess it up more

--- memory/main.js
+++ memory/main.js
@@ -1,9 +1,1 @@
 "use strict"
-
-
-// works kind of wonky... maybe with the avatar picture on the front it will work?
-$('.flip-card').on('click', function() {
-  $(this).css('transform', 'rotateY(180deg)');
-  console.log("this works");
-  console.log(this);
-})
