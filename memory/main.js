707bcc39f4c5b459f0edc38a028e6592ea293ee6
dave <d.gillespie221@gmail.com>
2019-03-21 21:53:15 -0400

    looking better! added javascript for flip

--- memory/main.js
+++ memory/main.js
@@ -1,1 +1,18 @@
 "use strict"
+
+const cards = document.querySelectorAll('.flip-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
