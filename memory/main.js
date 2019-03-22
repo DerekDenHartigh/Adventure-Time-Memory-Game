bee3efa9609dcd396dc3a665bca9176e955bd420
Derek <derekdenhartigh@gmail.com>
2019-03-21 21:04:56 -0400

    added cardRandomizer function and adventure time font

--- memory/main.js
+++ memory/main.js
@@ -1,5 +1,8 @@
 "use strict"
 $(()=>{
+    /* or this? for IIFE?
+$(document).ready(function() {
+    */
 
 $('.flip-card').on('click', function() {
   $(this).css('transform', 'rotateY(180deg)');
@@ -10,9 +13,24 @@ $('.flip-card').on('click', function() {
 
 /* card randomizer function here */
 
-const random = function(){
-    let n = 1;
+const cardRandomizer = function(){
+    let cardDivArray = [];
+    const createCardDivArray = function(){
+        $("div").each(function(){
+            if ($(this).hasClass("flip-card")===true){
+                cardDivArray.push(this);
+                return cardDivArray;
+            }
+        })
+    };
+    createCardDivArray();
 
+    console.warn(cardDivArray);
+
+    cardDivArray.forEach(function(n){
+        $(n).css("order", `${Math.floor(Math.random()*99)}`);
+    });
 };
-random()
+
+cardRandomizer()
 });
